bb664b9abe41c71fce5e080d49d7492eaab4cea9
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 03:24:53 -0400

    experimenting with layout and placement methods

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import tkinter as tk
 class MyApp:
     def __init__(self, root):
         root.title("My app")
-        root.geometry("800x600")
+        root.geometry("500x400")
         root.maxsize(1000, 800)
+
+        frame = Frame(root, width=200, height=100, borderwidth=2, relief="sunken")
+        frame.place(x=0, y=0)
 
         self.label_text = StringVar()
         label = Label(root, textvariable=self.label_text, font=("Arial", 24))
-        self.label_text.set("Hello, World!")
-        label.pack(side=TOP)
-        label2 = Label(root, text="This is a simple Tkinter app.", 
+        #self.label_text.set("Hello, World!")
+        #label.pack(side=LEFT, padx=10, pady=5)
+        #label.grid(column=1, row=1)
+        '''label2 = Label(root, text="This is a simple Tkinter app.", 
                        font=("Arial", 16), bg="lightblue")
-        label2.pack()
+        label2.pack(side=LEFT)'''
 
         #label["text"] = "New label text"
         #label["bg"] = "red"
@@ -22,22 +26,26 @@ class MyApp:
 
         self.entry_text = StringVar()
         entry = Entry(root, textvariable=self.entry_text)
-        entry.pack()
-
-
+        #entry.pack(side=LEFT)
+        #entry.place(x=100, y=50)
+        #entry.grid(column=3, row=1)
         #entry_text.set("Default text")
         #entry_text.get()
 
         #label["textvariable"] = entry_text
 
         button = Button(root, text="Click me!", command=self.press_button )
-        button.pack()
+        #button.pack(side=LEFT)
+        #button.grid(column=1, row=2, sticky=(S,E,W))
+        #button.place(x=0, y=0)
+        #button.configure(width=10, height=2, font=("Courier", 40))
 
         self.list_item_strings = ["Item 1", "Item 2", "Item 3"]
         self.list_items = StringVar(value=self.list_item_strings)
         listbox = Listbox(root, listvariable=self.list_items)
         listbox["height"] = 3
-        listbox.pack()
+        #listbox.pack(side=tk.LEFT, padx=40, pady=20)
+        #listbox.grid(column=2, row=2)
         listbox.bind("<<ListboxSelect>>", lambda event: self.select_item(listbox.curselection()))
         #can also just past the first item of the tuple
         #listbox.bind("<<ListboxSelect>>", lambda event: self.select_item(listbox.curselection()[0]))
