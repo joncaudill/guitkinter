51de7cbd863067f24e89caee6c1dffba7571747f
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-05-26 05:07:57 -0400

    add todo entry fields and labels

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class ToDoListApp:
         root.columnconfigure(1, weight=1)
         root.rowconfigure(1, weight=1)
 
+        list_label = Label(frame, text="All Items")
+        list_label.grid(column=1, row=1, sticky=(S, W))
+
         self.to_do_items = [
             ToDoItem("Buy groceries", "Milk, Bread, Eggs"),
             ToDoItem("Clean the house", "Living room, Kitchen, Bathroom"),
@@ -24,41 +27,54 @@ class ToDoListApp:
             ToDoItem("Walk the dog", "30 minutes in the park")
         ]
 
-        self.label_text = StringVar()
-        label = Label(frame, text="Some label text", textvariable=self.label_text)
-        label.pack()
+        self.to_do_names = StringVar(value=list(map(lambda item: item.name, self.to_do_items)))
 
-
-        label.configure(font=("Courier", 44), text="New label text")
-
-        self.entry_text = StringVar()
-        entry = Entry(frame, textvariable=self.entry_text)
-        entry.pack()
-
-
-        button = Button(frame, text="Button text", command=self.press_button)
-        button.pack()
-
-        self.list_item_strings = ["Hey", "Hi", "Hello", "Howdy", "Greetings"]
-        list_items = StringVar(value=self.list_item_strings)
-        listbox = Listbox(frame, listvariable=list_items)
-        listbox["height"] = 3
-        listbox.bind("<<ListboxSelect>>", lambda s: self.select_item(listbox.curselection()))
-        listbox.pack()
+        items_list = Listbox(frame, listvariable=self.to_do_names)
+        items_list.bind("<<ListboxSelect>>", lambda s: self.select_item(items_list.curselection()))
+        items_list.grid(column=1, row=2, sticky=(W, E), rowspan=5)
         #can also just past the first item of the tuple
-        #listbox.bind("<<ListboxSelect>>", lambda event: self.select_item(listbox.curselection()[0]))
+        #listbox.bind("<<ListboxSelect>>", lambda event: self.select_item(items_list.curselection()[0]))
 
-    def press_button(self):
-        #print("Button pressed!")
-        self.label_text.set(self.entry_text.get())
+        self.selected_description = StringVar()
+        selected_description_label = Label(frame, textvariable=self.selected_description, 
+                                           wraplength=200)
+        selected_description_label.grid(column=1, row=7, sticky=(W, E))
+
+        to_do_label = Label(frame, text="New Item")
+        to_do_label.grid(column=2, row=1, sticky=(S, W))
+
+        name_label = Label(frame, text="Item name", font='TkHeadingFont')
+        name_label.grid(column=2, row=2, sticky=(S, W))
+
+        self.name = StringVar()
+        name_entry = Entry(frame, textvariable=self.name)
+        name_entry.grid(column=2, row=3, sticky=(W, N, E))
+
+        description_label = Label(frame, text="Item description", font='TkHeadingFont')
+        description_label.grid(column=2, row=4, sticky=(S, W))
+
+        self.description = StringVar()
+        description_entry = Entry(frame, textvariable=self.description)
+        description_entry.grid(column=2, row=5, sticky=(W, N, E))
+
+        save = tk.Button(frame, text="Save", command=self.save_item)
+        save.grid(column=2, row=6, sticky=E)
+
+    def save_item(self):
+        new_item = ToDoItem(self.name.get(), self.description.get())
+        self.to_do_items.append(new_item)
+        self.to_do_names.set(list(map(lambda item: item.name, self.to_do_items)))
+        self.name.set("")
+        self.description.set("")
+        print("Item saved:", new_item.name, "-", new_item.description)
+
 
     def select_item(self, index):
-        #print("Item selected:", index)
         #if you only pass the first item of the tuple it needs to change to:
-        #selected_item = self.list_item_strings[index]
-        selected_item = self.list_item_strings[index[0]]
-        print("Item selected:", selected_item)
-
+        #selected_item = self.to_do_items[index]
+        selected_item = self.to_do_items[index[0]]
+        self.selected_description.set(selected_item.description)
+        print("Item selected:")
 
 root = Tk()
 ToDoListApp(root)
